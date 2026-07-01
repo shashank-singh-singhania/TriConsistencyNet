578071461871ae2aa2c50e6b3f522f6ba0c1bfee
Shashank Singh <145798216+shashank-singh-singhania@users.noreply.github.com>
2026-07-01 14:37:26 +0530

    Reduce prefetch_factor to 2 to prevent container RAM OOM

--- src/datasets/dataloader.py
+++ src/datasets/dataloader.py
@@ -97,7 +97,7 @@ def get_dataloaders(
         pin_memory=pin_memory,
         drop_last=True,
         persistent_workers=True if num_workers > 0 else False,
-        prefetch_factor=4 if num_workers > 0 else None,
+        prefetch_factor=2 if num_workers > 0 else None,
     )
 
     val_loader = DataLoader(
@@ -108,7 +108,7 @@ def get_dataloaders(
         pin_memory=pin_memory,
         drop_last=False,
         persistent_workers=True if num_workers > 0 else False,
-        prefetch_factor=4 if num_workers > 0 else None,
+        prefetch_factor=2 if num_workers > 0 else None,
     )
 
     test_loader = DataLoader(
@@ -119,7 +119,7 @@ def get_dataloaders(
         pin_memory=pin_memory,
         drop_last=False,
         persistent_workers=True if num_workers > 0 else False,
-        prefetch_factor=4 if num_workers > 0 else None,
+        prefetch_factor=2 if num_workers > 0 else None,
     )
 
     return train_loader, val_loader, test_loader
